ebe69a7258878978048791aa4841cc094122fec7
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-19 21:26:42 -0400

    Initial commit

---
+++ treeview.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+label = ttk.Label(root, text='Treeview')
+label.pack()
+
+treeview = ttk.Treeview(root)
+treeview.pack()
+
+
+
+
+
+treeview.insert('', '0', 'item1', text='First Item')
+treeview.insert('', '1', 'item2', text='Second Item')
+treeview.insert('', 'end', 'item3', text='Third Item')
+
+
+logo = PhotoImage(file='C:\\Users\\Paopao\\Pictures\\python.gif').subsample(10, 10)
+treeview.insert('item2', 'end', 'python', text='Python', image = logo)
+
+treeview.config(height=5)
+
+treeview.move('item2', 'item1', 'end')
+
+treeview.item('item1', open=True)
+
+
+if __name__ == '__main__':
+    mainloop()
